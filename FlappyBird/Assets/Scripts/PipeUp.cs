8b6c7df78598306df670991be11d05711d9a28cf
desulg <desulg@tlu.ee>
2019-04-28 18:46:11 +0300

    Functionality works

---
+++ FlappyBird/Assets/Scripts/PipeUp.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeUp : MonoBehaviour {
+
+    private Character character;
+    public GameObject pipeDown;
+
+    // Use this for initialization
+    void Start()
+    {
+        character = FindObjectOfType<Character>();
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (character.transform.position.x - transform.position.x > 30)
+        {
+
+            float xRan = Random.Range(0, 10);
+            float yRan = Random.Range(-5, 5);
+            float gapRan = Random.Range(0, 2);
+
+            Instantiate(gameObject, new Vector2(character.transform.position.x + 15 + xRan, -11 + yRan), transform.rotation);
+            Instantiate(pipeDown, new Vector2(character.transform.position.x + 15 + xRan, 12 + gapRan + yRan), transform.rotation);
+            Destroy(gameObject);
+
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            character.Death();
+        }
+    }
+
+}
